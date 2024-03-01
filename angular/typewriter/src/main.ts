16c61fc31217cba35c9867591b604bafea0cd870
Maarten Hus <maartenhus@gmail.com>
2024-03-01 16:22:00 +0100

    fix: angular examples did not work on StackBlitz
    The error that occurred was:
    
    `Error: NG0908: In this configuration Angular requires Zone.js`
    
    Fixed by manually importing zone.js in all `main.ts` files.

--- angular/typewriter/src/main.ts
+++ angular/typewriter/src/main.ts
@@ -1,4 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
+// Needed so StackBlitz works
+import 'zone.js';
+
 bootstrapApplication(AppComponent);
--- angular/view-channel/src/main.ts
+++ angular/view-channel/src/main.ts
@@ -3,6 +3,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 
+// Needed so StackBlitz works
+import 'zone.js';
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideAnimations()
